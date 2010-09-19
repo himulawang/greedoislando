bfce15d8de00fa9173c7ad460b87ded606c49803
Dya <Dya@localhost>
2010-09-19 17:21:58 +0800

    9-19 use_ap & deal card done

--- client/func.js
+++ client/func.js
@@ -1,41 +1,100 @@
 var ws1;
 
-var coreMap = {
-    "talk" : {
-        talk : "talk_talk"
-    },
-    "pre" : {
-        set_user_list : ""
-    }
-}
-
-/*
-var aa = '{"type":"pre","data":{"userlist":[{"id":"4c8de1f5baebb","username":"ila"},{"id":"4c8de1f9a807d","username":"Dya"}],"cmd":"set_user_list"}}';
-var json = JSON.parse(aa);
- */
-
-
 var _GI_ = {
     core : {
         globalProcess : function(msg){
-            var json = JSON.parse(msg);
-            var func = ["_GI_", json.type, json.data.cmd].join(".") + "(json)";
-            $("#console").html(func);
+            var json = JSON.parse(msg.data);
+            var string = msg.data;
+            output(string);
+            var func = ["_GI_", json.type, json.data.cmd].join(".") + "("+string+")";
             $.globalEval(func);
         }
     },
+
     talk : {
         talk : function(json){
             output(json.data.name + ": "+json.data.msg);
         } 
     },
 
-    pre : {
-        set_user_list : function(json){
-            output(json.data.userlist[0].username);
+    pre : {        
+        enter_bf : function(json){
+            for(x in json.data.char){
+                charname = $("#status_own > p:eq(1)").text();
+                if(json.data.char[x] == charname){
+                    $("#status_own > p:eq(1)").html("<p>" + json.data.char[x] + "</p>");
+                    $("#status_own > p:eq(2)").html("<p>" + x + "</p>");
+                }else{
+                    $("#status_oppo > p:eq(0)").html("<p>" + json.data.char[x] + "</p>");
+                    $("#status_oppo > p:eq(1)").html("<p>" + x + "</p>");
+                }
+            }
+        },
+        create_bf : function(json){
+            //创建战场
         }
-    }
+    },
 
+    con : {
+        get_id : function(json){
+            $("#status_own > p:eq(0)").html("<p>" + json.data.username + "<span class='id'>" + json.data.id + "</span></p>");
+        },
+
+        set_user_list : function(json){
+            var x;
+            $("#user_online_list").empty();
+            $("#user_online_list").append("<p>Online User List<span style='float:right;cursor:pointer;' onclick='list_online_player(2);'>X</span></p>");
+            for(x in json.data.userlist){
+                $("#user_online_list").append("<p>" + json.data.userlist[x].username + "</p>");
+            }
+        },
+
+        set_battlefieldlist : function(json){
+            var x;
+            $("#battle_field_online_list").empty();
+            $("#battle_field_online_list").append("<p>Online BF List<span style='float:right;cursor:pointer;' onclick='list_online_battle_field(2)'>X</span></p>");
+            for(x in json.data.battlefieldlist){
+                $("#battle_field_online_list").append("<p>Room No." + json.data.battlefieldlist[x].no + " " + json.data.battlefieldlist[x].bf_name + "<span style='float:right;cursor:pointer;' onclick='enter_bf("+ json.data.battlefieldlist[x].no +");'>Enter</span></p>");
+            }            
+        },
+
+    },
+
+    batt : {
+        set_action_point : function(json){
+            var x;
+            var ownid = $("#status_own > p:eq(2)").text();
+            var oppid = $("#status_oppo > p:eq(1)").text();
+            for(x in json.data.char){
+                if(x == ownid){
+                    var own_value = json.data.char[x] * 10;
+                    $("#own_ap").val(own_value);
+                }else if(x == oppid){
+                    var opp_value = json.data.char[x] * 10;
+                    $("#opp_ap").val(opp_value);
+                }
+            }
+        },
+
+        deal_cardo : function(json){
+            var x;
+            var ownid = $("#status_own > p:eq(2)").text();
+            var oppid = $("#status_oppo > p:eq(1)").text();
+            for(x in json.data.cardo){
+                var y;
+                if(x == ownid){
+                    for(y in json.data.cardo[x]){
+                        $("#cardo_own").append("<div style='border:1px solid #CCC;height:100px;width:75px;float:left;margin-left:5px;' onclick='use_actionpoint();'>" + json.data.cardo[x][y] + "</div>");
+                    }
+                }else if(x == oppid){
+                    for(y in json.data.cardo[x]){
+                        $("#cardo_oppo").append("<div style='border:1px solid #CCC;height:100px;width:75px;float:left;margin-left:5px;' onclick='use_actionpoint();'>" + json.data.cardo[x][y] + "</div>");
+                    }
+                }
+            }
+        }
+
+    }
 }
 
 function connect(){
@@ -70,6 +129,7 @@ function connect(){
     });    
     ws1.onopen = function(){
         output("Connected"); 
+        set_user_name();
         setInterval(
             function(){
                 if(ws1.bufferedAmount == 0){
@@ -132,16 +192,71 @@ function change_name(){
 }
 
 function create_battlefield(){
+    set_user_name();
+    var username = $("#user_name").val();
 	var char = $("#char_name").val();
     var bf_name = $("#bf_name").val();
     ws1.send("pre",{cmd:"create_bf",bf_name:bf_name,char_name:char});
+    $("#status_own > p:eq(1)").html("<p>" + char + "</p>");
 	$("#battle_field").css({display:"none"});
 	$("#battle_room").css({display:"block"});
-	$("#status_own").html(char);
 }
 
-function list_online_player(){
-    
+function list_online_player(state){
+    if(state==1){
+		if($("#user_online_list").css("display")=="none"){
+			$("#user_online_list").slideDown("normal");
+		}else{
+			$("#user_online_list").hide("fast",function(){
+				$("#user_online_list").slideDown("normal");
+			});			
+		}		
+	}else if(state==2){
+		$("#user_online_list").slideUp("normal");
+	}
+}
+
+function list_online_battle_field(state){
+    if(state==1){
+		if($("#battle_field_online_list").css("display")=="none"){
+			$("#battle_field_online_list").slideDown("normal");
+		}else{
+			$("#battle_field_online_list").hide("fast",function(){
+				$("#battle_field_online_list").slideDown("normal");
+			});			
+		}		
+	}else if(state==2){
+		$("#battle_field_online_list").slideUp("normal");
+	}
+}
+
+function enter_bf(num){
+	set_user_name();
+	var username = $("#user_name").val();
+	var char = $("#char_name").val();
+	var bf_no = num;
+	ws1.send("pre",{cmd:"enter_bf",bf_no:bf_no,char_name:char});
+    $("#status_own > p:eq(1)").html("<p>" + char + "</p>");
+	$("#battle_field").css({display:"none"});
+	$("#battle_room").css({display:"block"});
+}
+
+function bf_start(){
+    char = $("#status_own > p:eq(1)").text();
+    ws1.send("pre",{cmd:"start_bf"});
+}
+
+function chat(){
+    var el = $("#chat_input");
+    var char = $("#status_own > p:eq(1)").val();
+    var msg = el.val()
+    var json = {name:char,msg:msg}
+    ws1.send("talk",json);
+    el.val("");
+}
+
+function use_actionpoint(){
+    ws1.send("sys",{cmd:"use_actionpoint"});
 }
 
 $(function(){
