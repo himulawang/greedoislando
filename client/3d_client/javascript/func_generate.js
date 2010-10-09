bc83bbcd7d4402696a99e3cd113d2cf49b6af9f2
Dya <Dya@localhost>
2010-10-09 17:27:59 +0800

    10-9

--- client/3d_client/javascript/func_generate.js
+++ client/3d_client/javascript/func_generate.js
@@ -1,5 +1,27 @@
 function generate_login_interface(){
-	$("body").html(
-		"<div id='login'></div>"		
-	);
+    $("#islando").append("<div id='welcome-title'></div><div id='login'><div id='username'><p>USERNAME: </p><input type='text' id='username' class='text_input' value='' /></div><div id='username'><p>PASSWORD: </p><input type='password' id='password' class='text_input' value='' /></div><div id='login_func_button'><input type='button' value='LOGIN' class='button index_login'/><input type='button' value='CANCEL' class='button index_login' onclick='active(\"welcome-title\");'/></div></div>");
 }
+
+var msg = "This is a Game produced by ilaempire members. Welcome to our world [Greed Island]. Enjoy urselves and have fun!";
+
+function text_generate_obo(msg,id){
+    var printed_text_length = $("#" + id).html().length;
+    if(printed_text_length >= msg.length){
+        window.clearInterval(s);
+        return;
+    }
+
+    char = msg.charAt(printed_text_length);
+    text_generate(char,id);
+}
+
+function active(id){
+    var s = setInterval("text_generate_obo(msg,'"+id+"')",10);
+}
+
+function text_generate(char,id){
+    $("#" + id).append(char);
+}
+
+
+
