64668d9efd833930f5055d31963d0cb795cc9a47
ila <ila@localhost>
2011-08-25 21:58:54 +0800

    Client: add skill to lower;

---
+++ Client V0.04/script/skill/skill_morafabric.js
@@ -0,0 +1,5 @@
+var Skill_MoraFabric = function(skillID) {
+    Skill_MoraFabric.super_.apply(this, arguments);
+}
+
+util.inherits(Skill_MoraFabric, Skill);
---
+++ Client V0.04/script/skill/skill_morascissors.js
@@ -0,0 +1,5 @@
+var Skill_MoraScissors = function(skillID) {
+    Skill_MoraScissors.super_.apply(this, arguments);
+};
+
+util.inherits(Skill_MoraScissors, Skill);
